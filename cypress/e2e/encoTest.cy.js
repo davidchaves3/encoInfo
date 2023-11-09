73cea267656ecd33ac49301d580d06ab5e51cc6a
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2023-11-08 21:48:00 -0300

    addcommands

--- cypress/e2e/encoTest.cy.js
+++ cypress/e2e/encoTest.cy.js
@@ -1,38 +1,22 @@
-describe('Testando...', () => {
-  it('Transações...',()=>{
-    cy.visit('https://devfinance-agilizei.netlify.app/')
-    cy.get('header > img').should('be.visible')
+import commands from '../support/commands'
+
+  describe('Manter Transações', () =>{
+    beforeEach(()=>{
+      cy.acessaDevFinances()
+    })
+    it('Excluir transção...',()=>{
+      cy.get('#transaction > .button').click()
+      cy.get('#form > h2').contains('Nova Transação')
+
+      cy.get('#description').type('Primeira Transação')
+      cy.get('#amount').type('200.000')
+      cy.get('#date').type('2023-10-12')
+      cy.get('button').contains('Salvar').click()
+
+      //cy.get(':nth-child(4) > img').click()
+      cy.contains('.description','Primeira Transação').parent().find('img').click()
   });
 
-    it('Manter Transações...',()=>{
-    cy.visit('https://devfinance-agilizei.netlify.app/')
-    cy.get('header > img').should('be.visible')
-
-    cy.get('#transaction > .button').click()
-    cy.get('#form > h2').contains('Nova Transação')
-
-    cy.get('#description').type('Primeira Transação')
-    cy.get('#amount').type('200.000')
-    cy.get('#date').type('2023-10-12')
-    cy.get('button').contains('Salvar').click()
-
+  })
 
     
-  });
-    it(('Excluir transção...'),()=>{
-        cy.visit('https://devfinance-agilizei.netlify.app/')
-        cy.get('header > img').should('be.visible')
-
-        cy.get('#transaction > .button').click()
-        cy.get('#form > h2').contains('Nova Transação')
-
-        cy.get('#description').type('Primeira Transação')
-        cy.get('#amount').type('200.000')
-        cy.get('#date').type('2023-10-12')
-        cy.get('button').contains('Salvar').click()
-
-        //cy.get(':nth-child(4) > img').click()
-        cy.contains('.description','Primeira Transação').parent().find('img').click()
-    });
-
-})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,15 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('acessaDevFinances', ()=>{
+  cy.visit('https://devfinance-agilizei.netlify.app/')
+  cy.get('header > img').should('be.visible')
+});
+
+
+Cypress.Commands.add('addTransação', (descricao, valor, data)=>{
+  cy.get('#transaction > .button').click()
+  cy.get('#form > h2').contains('Nova Transação')
+
+  cy.get('#description').type(descricao)
+  cy.get('#amount').type(valor)
+  cy.get('#date').type(data)
+  cy.get('button').contains('Salvar').click()
+});
