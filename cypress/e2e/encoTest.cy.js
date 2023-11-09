f096e7c7f566876a99967d00ee320395815cf699
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2023-11-08 21:13:36 -0300

    excluindo

--- cypress/e2e/encoTest.cy.js
+++ cypress/e2e/encoTest.cy.js
@@ -15,5 +15,24 @@ describe('Testando...', () => {
     cy.get('#amount').type('200.000')
     cy.get('#date').type('2023-10-12')
     cy.get('button').contains('Salvar').click()
+
+
+    
   });
+    it(('Excluir transção...'),()=>{
+        cy.visit('https://devfinance-agilizei.netlify.app/')
+        cy.get('header > img').should('be.visible')
+
+        cy.get('#transaction > .button').click()
+        cy.get('#form > h2').contains('Nova Transação')
+
+        cy.get('#description').type('Primeira Transação')
+        cy.get('#amount').type('200.000')
+        cy.get('#date').type('2023-10-12')
+        cy.get('button').contains('Salvar').click()
+
+        //cy.get(':nth-child(4) > img').click()
+        cy.contains('.description','Primeira Transação').parent().find('img').click()
+    });
+
 })
